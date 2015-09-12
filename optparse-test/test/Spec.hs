513e7d8cfd83b831800d41daccb5c419d864c9b9
Tristan Webb <tristan@fpcomplete.com>
2015-09-12 00:59:49 -0700

    Basic tests

--- optparse-test/test/Spec.hs
+++ optparse-test/test/Spec.hs
@@ -1,2 +1,39 @@
+module Main where
+
+import Test.Hspec
+import System.IO
+import System.Process
+import System.Exit
+import Control.Monad.IO.Class
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+
+
+main =
+    hspec $
+    before stackBuild $
+    do describe "Optparse tests" $
+           do it "No options case" $
+                  do exit <- runExe [""]
+                     do exit `shouldBe` ExitSuccess
+              it "foo base command case" $
+                  do exit <- runExe ["foo"]
+                     do exit `shouldBe` ExitSuccess
+              it "foo base command case" $
+                  do exit <- runExe ["--bar", "foo"]
+                     do exit `shouldBe` ExitSuccess
+
+stackBuild :: IO ()
+stackBuild =
+    callProcess
+        "stack"
+        ["build"]
+
+runExe :: [String] -> IO ExitCode
+runExe options = do
+    (exitCode,_,_) <-
+        readProcessWithExitCode
+            "stack"
+            (["exec", "--", "optparse-test-exe"] ++
+             options) ""
+    return exitCode
